03d76177305a53daecb00eeeee7727b5e4eee581
Jeison Franco <stfrancoc@gmail.com>
2023-03-08 20:52:49 -0500

    se agregaron los localservice para la creacion de claves

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany, belongsTo} from '@loopback/repository';
+import {belongsTo, Entity, hasMany, model, property} from '@loopback/repository';
 import {Login} from './login.model';
 import {Rol} from './rol.model';
 
@@ -49,7 +49,7 @@ export class Usuario extends Entity {
     type: 'string',
     required: true,
   })
-  clave: string;
+  clave?: string;
 
   @hasMany(() => Login)
   logins: Login[];
